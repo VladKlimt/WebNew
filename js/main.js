678dd6430f0ff3bb2d7d0d43f4a248f3e36fa21e
OlehDev <Olehifkep@gmail.com>
2017-12-29 16:29:36 +0200

    Final

--- js/main.js
+++ js/main.js
@@ -32,15 +32,11 @@ $(document).ready(function() {
 	});
 
 
-
-
-
-
   /*product*/
   $('#exampleModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) /
   var recipient = button.data('whatever')
-  
+
   var modal = $(this)
   modal.find('.modal-title').text('New message to ' + recipient)
   modal.find('.modal-body input').val(recipient)
